9807f9f53007e3914abb4cac0807016fbb23ce8f
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-11-19 14:27:08 +1100

    add playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let compScore = 0
+
 function getCompChoice() {
     let choice = Math.random()
     if (choice <= 0.33) {
@@ -14,4 +17,46 @@ function getCompChoice() {
 function getHumanChoice() {
     let choice = prompt("Make a throw (rock, paper, or scissors): ").toLowerCase();
     return choice;
+}
+
+function playRound(humanChoice, compChoice) {
+    if (humanChoice == "rock") {
+        if (compChoice == "rock") {
+            console.log("It's a tie!");
+        } 
+        else if (compChoice == "paper") {
+            console.log("You lose! Paper beats rock!");
+            compScore++;
+        }
+        else {
+            console.log("You win! Rock beats scissors!");
+            humanScore++;
+        }
+    }
+    else if (humanChoice == "paper") {
+        if (compChoice == "paper") {
+            console.log("It's a tie!");
+        } 
+        else if (compChoice == "scissors") {
+            console.log("You lose! Scissors beats paper!");
+            compScore++;
+        }
+        else {
+            console.log("You win! Paper beats rock!");
+            humanScore++;
+        }
+    }
+    else {
+        if (compChoice == "scissors") {
+            console.log("It's a tie!");
+        } 
+        else if (compChoice == "rock") {
+            console.log("You lose! Rock beats scissors!");
+            compScore++;
+        }
+        else {
+            console.log("You win! Scissors beats paper!");
+            humanScore++;
+        }
+    }
 }
