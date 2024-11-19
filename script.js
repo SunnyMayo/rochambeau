979aa982b1c149dce941ba570ff46840d64eca38
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-11-19 14:16:25 +1100

    add getCompChoice function

---
+++ script.js
@@ -0,0 +1,12 @@
+function getCompChoice() {
+    let choice = Math.random()
+    if (choice <= 0.33) {
+        return "rock";
+    }
+    else if (choice < 0.67) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
